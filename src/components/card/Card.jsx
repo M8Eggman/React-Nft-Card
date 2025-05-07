8cbb38db4e3cf4f03f970a138b70682f1f3347fc
GoNEAR <cagatay.morgul@outlook.com>
2025-05-07 14:15:26 +0200

    structure card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,29 @@
+import "./Card.css";
+import userImg from "../../assets/images/image-avatar.png"
+import view from "../../assets/images/icon-view.svg"
+import eth from "../../assets/images/icon-ethereum.svg"
+import clock from "../../assets/images/icon-clock.svg"
+
+function Card() {
+  return <div className="card">
+    <div className="header">
+        <a href=""><img src="" alt="" /></a>
+    </div>
+    <div className="body">
+        <a href=""></a>
+        <p></p>
+        <div>
+            <p><img src="" alt="" /></p>
+            <p><img src="" alt="" /></p>
+        </div>
+    </div>
+    <div className="footer">
+        <div>
+            <img src="" alt="" />
+        </div>
+        <p><span></span></p>
+    </div>
+  </div>;
+}
+
+export default Card;
